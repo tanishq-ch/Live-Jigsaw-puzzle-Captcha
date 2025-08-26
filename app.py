08cc1488624742445dc0f3463b517a4d0f7a6a90
TANISHQ CHAUHAN <toshu2507.04@gmail.com>
2025-08-27 00:24:28 +0530

    Update app.py with improved server logic

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
     app.run(debug=True)
